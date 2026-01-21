5b7be82d341224c0538071956f4b40943655251c
dvrksyde <kzazizbek20@gmail.com>
2026-01-21 23:11:31 +0500

    Create nums.py

---
+++ nums.py
@@ -0,0 +1,19 @@
+import random
+
+x = 1
+y = 2.8
+z = 1j
+
+a = float(x)
+b = int(y)
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(random.randrange(1, 10))
