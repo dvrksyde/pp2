b612d3de5b0dc288e5246f6033c07574b6816f5e
dvrksyde <kzazizbek20@gmail.com>
2026-01-21 23:11:29 +0500

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,30 @@
+x = "Hello World"	
+y = 20
+z = 20.5	
+d = 1j		
+s = ["apple", "banana", "cherry"]	
+u = ("apple", "banana", "cherry")
+o = range(6)
+f = {"name" : "John", "age" : 36}
+p = {"apple", "banana", "cherry"}
+l = frozenset({"apple", "banana", "cherry"})
+m = True
+n = b"Hello"
+q = bytearray(5)
+e = memoryview(bytes(5))
+k = None
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(d))
+print(type(s))
+print(type(u))
+print(type(o))
+print(type(f))                                                                  
+print(type(p))
+print(type(l))
+print(type(m))
+print(type(n))
+print(type(q))
+print(type(e))
+print(type(k))
