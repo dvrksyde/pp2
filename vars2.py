1a077c42d750297ae69097a2cf7633289182386d
dvrksyde <kzazizbek20@gmail.com>
2026-01-21 15:52:54 +0500

    Update vars2.py

--- vars2.py
+++ vars2.py
@@ -7,9 +7,19 @@ x = y = z = "Orange"
 print(x)
 print(y)
 print(z)
-print("\n")
+print("\n") 
 fruits = ["Apple", "Banana", "Cherry"]
 x, y, z = fruits
 print(x)
 print(y)
 print(z)
+print("\n")
+print(x+y+z)
+print(x*3 + y*3 + z*3)
+print("\n")
+
+def my_function():
+    print(x, "is delicious")
+    print(y, "is fresh")
+    print(z, "is juicy")
+my_function()
