4537efb3882ebed38433feea8ad7af21ae92f98e
dvrksyde <kzazizbek20@gmail.com>
2026-01-21 15:49:23 +0500

    Create vars2.py

---
+++ vars2.py
@@ -0,0 +1,15 @@
+x,y,z = "Apple", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+print("\n")
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+print("\n")
+fruits = ["Apple", "Banana", "Cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
