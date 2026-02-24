7f62c8e70bf34934ac515d1f0f9c411cf8b5bd62
Aziz <kzazizbek20@gmail.com>
2026-02-24 15:01:03 +0500

    Done Tasks of Generators

--- Practice04/generator_expr.py
+++ Practice04/generator_expr.py
@@ -1,13 +1,28 @@
-string = "geek"
-
-reverse_gen = (string[i] for i in range(len(string) - 1, -1, -1))
-reverse_list = list(reverse_gen)
-
-print(reverse_list)
+def square(n):
+    for i in range(n):
+        yield i * i
 
 
-squares_list = [x * x for x in range(5)]
-squares_gen = (x * x for x in range(5))
+def even(n):
+    for i in range(n):
+        if i % 2 == 0:
+            yield i
 
-print(squares_list)
-print(list(squares_gen))
+
+def is_devisible(n):
+    for i in range(n):
+        if i % 2 == 0 and i % 3 == 0:
+            yield i
+
+
+def squared(n, m):
+    for i in range(n, m + 1):
+        yield i * i
+
+
+def up_to_down(n):
+    for i in range(n, -1, -1):
+        yield i
+
+
+n = int(input())
